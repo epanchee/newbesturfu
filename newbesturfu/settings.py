469e79ed2a5c41d717142e6992a84654d8e87cbc
Timofey <epanchee@gmail.com>
2015-08-20 13:03:23 +0500

    add deploy config

--- newbesturfu/settings.py
+++ newbesturfu/settings.py
@@ -97,7 +97,7 @@ MANAGERS = ADMINS
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -299,9 +299,6 @@ OPTIONAL_APPS = (
 ###################
 # DEPLOY SETTINGS #
 ###################
-
-# Domains for public site
-ALLOWED_HOSTS = ["*"]
 
 # These settings are used by the default fabfile.py provided.
 # Check fabfile.py for defaults.
