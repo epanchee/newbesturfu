68b7723515d5065753b8151beb71604776ac2279
Timofey <epanchee@gmail.com>
2015-08-20 12:57:25 +0500

    add deploy config

--- newbesturfu/settings.py
+++ newbesturfu/settings.py
@@ -157,7 +157,22 @@ FILE_UPLOAD_PERMISSIONS = 0o644
 # DATABASES #
 #############
 
-DATABASES = {}
+DATABASES = {
+    "default": {
+        # Add "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        # DB name or path to database file if using sqlite3.
+        "NAME": "",
+        # Not used with sqlite3.
+        "USER": "",
+        # Not used with sqlite3.
+        "PASSWORD": "",
+        # Set to empty string for localhost. Not used with sqlite3.
+        "HOST": "",
+        # Set to empty string for default. Not used with sqlite3.
+        "PORT": "",
+        }
+}
 
 
 # Parse database configuration from $DATABASE_URL
