51fbcff384a0a9a0f0eb272aec8ad239eae49fba
Timofey <epanchee@gmail.com>
2015-08-20 14:21:19 +0500

    add deploy config

--- newbesturfu/settings.py
+++ newbesturfu/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = "en"
+LANGUAGE_CODE = "ru"
 
 # Supported languages
 _ = lambda s: s
