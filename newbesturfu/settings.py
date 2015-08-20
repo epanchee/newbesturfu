549087c21df88760f1060d9e4b367a504cf2ca49
Timofey <epanchee@gmail.com>
2015-08-20 13:02:41 +0500

    add deploy config

--- newbesturfu/settings.py
+++ newbesturfu/settings.py
@@ -77,6 +77,7 @@ from __future__ import absolute_import, unicode_literals
 
 # If True, the south application will be automatically added to the
 # INSTALLED_APPS setting.
+from mezzanine.project_template.project_name.settings import DATABASES
 import customApp
 
 USE_SOUTH = True
@@ -156,24 +157,6 @@ FILE_UPLOAD_PERMISSIONS = 0o644
 #############
 # DATABASES #
 #############
-
-DATABASES = {
-    "default": {
-        # Add "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        # DB name or path to database file if using sqlite3.
-        "NAME": "",
-        # Not used with sqlite3.
-        "USER": "",
-        # Not used with sqlite3.
-        "PASSWORD": "",
-        # Set to empty string for localhost. Not used with sqlite3.
-        "HOST": "",
-        # Set to empty string for default. Not used with sqlite3.
-        "PORT": "",
-        }
-}
-
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
