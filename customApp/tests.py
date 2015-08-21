0af55c011273d88939af830cb61dca6a94ad167a
Timofey <epanchee@gmail.com>
2015-08-21 19:47:30 +0500

    add unworking test

--- customApp/tests.py
+++ customApp/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.test import TestCase, Client
+from mezzanine.pages import views
+from customApp.models import DownloadedPage
 
-# Create your tests here.
+
+class DownloadedPageTests(TestCase):
+
+    client = Client()
+
+    #TODO почему-то тест вылетает, а ошибка пишется в какой-то кривой кодировке
+    def netest_for_downloaded_data(self):
+        for dpage in DownloadedPage.objects.all():
+            response = self.client.get(reverse(views.page), kwargs={'slug', dpage.slug})
+            self.assertEqual(response.status_code, 200)
