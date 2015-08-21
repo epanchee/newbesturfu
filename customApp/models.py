6b2d05a73f3bb90cfb4ec0408a1b35ae89c1f6a7
Timofey <epanchee@gmail.com>
2015-08-21 17:23:05 +0500

    add translations for customApp admin page

--- customApp/models.py
+++ customApp/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.http import Http404
+from django.utils.translation import ugettext
 from mezzanine.pages.models import Page
 import urllib
 
 
 class DownloadedPage(Page):
-    dlink = models.TextField("dlink")
+    dlink = models.TextField(name="dlink", verbose_name=ugettext('Download link'))
 
     def download(self):
         f = urllib.urlopen(self.dlink)
