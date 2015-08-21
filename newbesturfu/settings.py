29e68ff6dd71d82caaa5d4b2aa97462abfe3f585
Timofey <epanchee@gmail.com>
2015-08-21 16:12:53 +0500

    add locale support

--- newbesturfu/settings.py
+++ newbesturfu/settings.py
@@ -215,6 +215,10 @@ ROOT_URLCONF = "%s.urls" % PROJECT_DIRNAME
 # Don't forget to use absolute paths, not relative paths.
 TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, "templates"),)
 
+LOCALE_PATHS = (
+    os.path.join(PROJECT_ROOT, "locale"),
+)
+
 
 ################
 # APPLICATIONS #
