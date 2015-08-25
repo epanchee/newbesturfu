69cc1cfd13aa927e2ecefd574481ef37ac601f43
Timofey <epanchee@gmail.com>
2015-08-25 22:55:54 +0500

    some fixes for email conf

--- newbesturfu/settings.py
+++ newbesturfu/settings.py
@@ -179,7 +179,10 @@ import os
 try:
     EMAIL_HOST = os.environ['EMAIL_HOST']
     EMAIL_PORT = os.environ['EMAIL_PORT']
-    EMAIL_USE_SSL = os.environ['EMAIL_USE_SSL']
+    if os.environ['EMAIL_USE_SSL']:
+        EMAIL_USE_SSL = True
+    if os.environ['EMAIL_USE_TLS']:
+        EMAIL_USE_TLS = True
     EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
     EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 except:
