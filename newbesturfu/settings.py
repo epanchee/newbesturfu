7c5a7f1282530cda1afd582a99177c82b01c38bf
Timofey <epanchee@gmail.com>
2015-08-25 21:39:13 +0500

    add email support

--- newbesturfu/settings.py
+++ newbesturfu/settings.py
@@ -185,7 +185,6 @@ import os
 
 EMAIL_HOST = os.environ['EMAIL_HOST']
 EMAIL_PORT = os.environ['EMAIL_PORT']
-EMAIL_USE_TLS = os.environ['EMAIL_USE_TLS']
 EMAIL_USE_SSL = os.environ['EMAIL_USE_SSL']
 EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
