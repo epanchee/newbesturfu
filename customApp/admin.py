3e0426cc5065fe101dfb2cebe941797b557142e0
root <root@ip-172-31-38-81.us-west-2.compute.internal>
2016-03-22 16:16:11 -0400

    removed unworking decorator for PartnerAdmin

--- customApp/admin.py
+++ customApp/admin.py
@@ -3,9 +3,9 @@ from mezzanine.pages.admin import PageAdmin
 from .models import DownloadedPage, Partner
 
 
-@admin.register(Partner)
 class PartnerAdmin(admin.ModelAdmin):
     list_display = ('partners_name', 'logo_url',)
 
 
+admin.site.register(Partner, PartnerAdmin)
 admin.site.register(DownloadedPage, PageAdmin)
